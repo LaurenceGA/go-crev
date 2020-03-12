2ccf650fb8917da19245e70d0a0beed8b7c1a56e
LaurenceGA <lorryarmstrong@gmail.com>
2020-03-12 21:38:40 +0900

    Updated command name

--- command/root.go
+++ command/root.go
@@ -4,13 +4,15 @@ import "github.com/spf13/cobra"
 
 func InitialiseRootCommand() *cobra.Command {
 	return &cobra.Command{
-		Use:   "grev",
-		Short: "A brief description of your application",
-		Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+		Use:   "gocrev",
+		Short: "A cryptographically verifiable code review system for go packages.",
+		Long: `gocrev is a social code review system.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+This is an implementation of the language agnostic crev.
+For more information see here: https://github.com/crev-dev/crev
+
+gocrev is in the early stages of development, please raise
+issues here: https://github.com/LaurenceGA/go-crev/issues
+`,
 	}
 }
