5a63a34527af7fb221d456eef5259410c49d545a
LaurenceGA <lorryarmstrong@gmail.com>
2020-03-15 00:43:53 +0900

    Generate go version in mage build

--- magefile.go
+++ magefile.go
@@ -3,7 +3,11 @@
 package main
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"time"
 
 	"github.com/magefile/mage/sh"
@@ -16,11 +20,54 @@ func Build() error {
 }
 
 func ldFlagsArg() string {
-	return constructLDFlags(getBuildDate(), "1.2.5")
+	buildTime := getBuildTime()
+	version := getVersion()
+
+	log.Printf("Setting builtTime to %s\n", buildTime)
+	log.Printf("Setting version to %s\n", version)
+
+	return constructLDFlags(buildTime, version)
 }
 
-func getBuildDate() string {
+func getBuildTime() string {
 	return time.Now().Format(time.RFC3339)
+}
+
+const gitNoTagsStatus = 128
+
+func getVersion() string {
+	latestTag, err := getLatestTag()
+	if err != nil {
+		if sh.ExitStatus(errors.Unwrap(err)) == gitNoTagsStatus {
+			log.Println("No tags found")
+
+			return getShortHash()
+		} else {
+			log.Fatal(err)
+		}
+	}
+
+	return fmt.Sprintf("%s-%s", latestTag, getShortHash())
+}
+
+// gets the latest tag for this branch
+func getLatestTag() (string, error) {
+	var buf bytes.Buffer
+	_, err := sh.Exec(nil, &buf, ioutil.Discard, "git", "describe", "--abbrev=0", "--tags")
+	if err != nil {
+		return "", fmt.Errorf("retrieving tag: %w", err)
+	}
+
+	return buf.String(), nil
+}
+
+func getShortHash() string {
+	output, err := sh.Output("git", "rev-parse", "--short", "HEAD")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return output
 }
 
 func constructLDFlags(buildTime, version string) string {
