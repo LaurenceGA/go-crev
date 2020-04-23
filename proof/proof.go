3bd09feaf881b1a72fa785c40f85e8f8a70476e2
LaurenceGA <lorryarmstrong@gmail.com>
2020-04-23 23:27:44 +0900

    Start create proof with common fields

---
+++ proof/proof.go
@@ -0,0 +1,21 @@
+package proof
+
+import (
+	"time"
+
+	"github.com/LaurenceGA/go-crev/internal/id"
+)
+
+type Kind string
+
+const (
+	Trust  Kind = "trust"
+	Review Kind = "package review"
+)
+
+type CommonData struct {
+	Kind    Kind      `yaml:"kind"`
+	Version int       `yaml:"version"`
+	Date    time.Time `yaml:"date"`
+	From    id.ID     `yaml:"from"`
+}
